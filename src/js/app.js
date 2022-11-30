822a43e55b8213e1148d676ed852b8c30e88c64a
Martin <mdsugarev@gmail.com>
2022-12-01 00:33:24 +0200

    added li elements

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,22 @@
 import "../scss/app.scss";
+const url = 'https://pokeapi.co/api/v2/pokemon?limit=10'
 
 window.addEventListener("DOMContentLoaded", () => {
-  // This block will be executed once the page is loaded and ready
-
   const ul = document.querySelector("ul");
+  fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    const arrayOfPok = data.results
+    arrayOfPok.map(pokemon => {
+      const li = document.createElement("li")
+      li.innerText = pokemon.name
+      ul.appendChild(li)
+    })
+  }) 
 });
+
+
+
+
+
+
